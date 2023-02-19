058f35b7397df6b4c4a602fae5235a0561f046af
Daniel Posso <samauricioperez@gmail.com>
2023-02-19 13:51:57 -0500

    version3.0

--- clase10_semana2_flexboxGrids/js/basic.js
+++ clase10_semana2_flexboxGrids/js/basic.js
@@ -1,2 +1,1 @@
-alert("A continuación verás la semana 2 del bootcamp BIT|QSD")
-let mensaje = confirm("Dale 'Aceptar' si querés ver la página")
+alert("A continuación verás la semana 2 del bootcamp BIT|QSD");
